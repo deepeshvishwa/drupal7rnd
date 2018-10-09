6d464f4dcc657b9b43ae2de36536c7274f2d2fb2
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-10-09 20:00:02 +0530

    update custom module

--- sites/all/modules/loggedin_users_list/loggedin-users-list.tpl.php
+++ sites/all/modules/loggedin_users_list/loggedin-users-list.tpl.php
@@ -5,5 +5,5 @@
  * Show loggedin user list.
  */
 ?>
-<table> <tr> <td><b>Currently number of loggedin users:</b></td> <td><b><?php print $users_no;?></b></td> </tr> </table>
+<table> <tr> <td><b>Currently number of logged-in users:</b></td> <td><b><?php print $users_no;?></b></td> </tr> </table>
 <?php print $userdata;?>
