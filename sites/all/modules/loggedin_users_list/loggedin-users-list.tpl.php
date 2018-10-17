3c585166b6829752afb8ec0bc775ba2fd408bdff
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-10-17 20:36:22 +0530

    upload some changes

--- sites/all/modules/loggedin_users_list/loggedin-users-list.tpl.php
+++ sites/all/modules/loggedin_users_list/loggedin-users-list.tpl.php
@@ -5,5 +5,5 @@
  * Show logged-in user list.
  */
 ?>
-<table> <tr> <td><b>Currently number of logged-in users:</b></td> <td><b><?php print $users_no;?></b></td> </tr> </table>
+<table> <tr> <td><b>Currently number of logged-in users:</b></td> <td><b><?php print check_plain($users_no);?></b></td> </tr> </table>
 <?php print $userdata;?>
